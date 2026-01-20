6dcf626c11ab7f6ed02777a9e71f2e3a8ace019b
SRLoveys <147052904+SRLoveys@users.noreply.github.com>
2026-01-20 09:27:30 -0330

    Update rover.js

--- Assignment_1/ch14_project_exercises/mars_rover/rover.js
+++ Assignment_1/ch14_project_exercises/mars_rover/rover.js
@@ -24,8 +24,45 @@ document.addEventListener("DOMContentLoaded", async () => {
         
     getElement("#options").classList.remove("hide");
 
-    
+    getElement("#status").textContent = roverList[0].status;
+    getElement("#photos").textContent = roverList[0].total_photos;
+    getElement("#landing").textContent = roverList[0].landing_date
+    getElement("#max").textContent = roverList[0].max_date;
 
+    const yearSelect = getElement("#year");
+    for(let i=2026; i>1999; i--) {
+        let option = document.createElement("option")
+        option.value = i;
+        option.textContent = i;
+        yearSelect.appendChild(option);
+    }
+
+    const monthSelect = getElement("#month");
+    for(let i=1; i<=12; i++) {
+        let option = document.createElement("option")
+        option.value = i;
+        option.textContent = i;
+        monthSelect.appendChild(option);
+    }
+
+    const daySelect = getElement("#date");
+    for(let i=1; i<=31; i++) {
+        let option = document.createElement("option")
+        option.value = i;
+        option.textContent = i;
+        daySelect.appendChild(option);
+    }
+
+    const cameraSelect = getElement("#camera");
+
+    const cameraData = roverList[0].cameras;
+
+    cameraData.forEach( camera => {
+        let option = document.createElement("option")
+        option.value = camera.name;
+        option.textContent = camera.name;
+        cameraSelect.appendChild(option)
+    })
 
 
 
